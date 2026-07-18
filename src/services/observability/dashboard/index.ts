17625875e7dd1ba2600769e621e1705f31d2e421
Willyodas <will.j.evans1@gmail.com>
2026-07-18 12:34:37 +0000

    feat: protect observability dashboard

--- src/services/observability/dashboard/index.ts
+++ src/services/observability/dashboard/index.ts
@@ -1,4 +1,5 @@
 export * from "./aggregate-operational-events";
+export * from "./dashboard-auth";
 export * from "./get-observability-dashboard";
 export * from "./reporting-window";
 export * from "./types";
